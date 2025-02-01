e6a84a79b4b6ee17bf634343703b6d5a6101f511
jackdtx <evgen23.06.1998@gmail.com>
2025-02-01 20:30:26 +0300

    problems with main

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,17 @@ package cmd
 
 import (
 	"TgNewsPet/config"
+	fetcher "TgNewsPet/fetcher"
+	"TgNewsPet/notifier"
+	"TgNewsPet/storage"
+	"context"
+	"errors"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/jmoiron/sqlx"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -14,6 +22,43 @@ func main() {
 		return
 	}
 
-	db, err := sqlx.Connect("postgres")
+	db, err := sqlx.Connect("postgres", config.Get().DataBaseDSN)
+	if err != nil {
+		log.Printf("failed to connect to database: %v", err)
+		return
+	}
+	defer db.Close()
 
+	var (
+		articleStorage = storage.NewArticleStorage(db)
+		sourceStorage  = storage.NewSourceStorage(db)
+		fetcher        = fetcher.New(
+			articleStorage,
+			sourceStorage,
+			config.Get().FetchInterval,
+			config.Get().FilterKeywords,
+		)
+		notifier = notifier.New(
+			articleStorage,
+			summarizer,
+			botAPI,
+			config.Get().NotificationInterval,
+			2*config.Get().FetchInterval,
+			config.Get().TelegramChannelID,
+		)
+	)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	go func(ctx context.Context) {
+		if err := fetcher.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("[ERROR] failed to run fetcher: %v", err)
+				return
+			}
+
+			log.Printf("[INFO] fetcher stopped")
+		}
+	}(ctx)
 }
